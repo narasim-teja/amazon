54d228776327234bf7ae54b4b02e60981012ddba
Vijay <Vijay.8085@gmail.com>
2020-09-11 18:08:48 +0530

    first

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Home.css";
+import SearchIcon from '@material-ui/icons/Search';
+function Home() {
+    return (
+        <div className="home">
+            <SearchIcon />
+        </div>
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwsY7PLuZhsUnd_PHeHnLLMN_KbH76ifs",
+    authDomain: "challenge-12ff7.firebaseapp.com",
+    databaseURL: "https://challenge-12ff7.firebaseio.com",
+    projectId: "challenge-12ff7",
+    storageBucket: "challenge-12ff7.appspot.com",
+    messagingSenderId: "481620930700",
+    appId: "1:481620930700:web:86734cc02fbc359aebcba9",
+    measurementId: "G-T9P32PXELQ"
+  };
